cf6580385be3205549baf1f04e11da2108a47e4e
unsivil <unsivilaudio@gmail.com>
2020-09-15 09:40:33 -0600

    implement base Redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+import reducers from './reducers';
 import './assets/stylesheets/main.css';
 import App from './components/App';
 
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );
